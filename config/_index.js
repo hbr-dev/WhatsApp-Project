40ff13959a0c3b62ab4638e4047cd06a6372a607
hbr-dev <dev.csm.2022@gmail.com>
2024-12-06 13:18:06 +0100

    WhatsApp ReactNative Mini project upload: Initial commit

---
+++ config/_index.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import app from 'firebase/compat/app';
+import "firebase/compat/auth";
+import "firebase/compat/database";
+import "firebase/compat/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+
+const firebaseApp = app.initializeApp(firebaseConfig);
+export default firebaseApp;
